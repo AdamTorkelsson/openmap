a5ae31143b09fd79a9202034e80f4b4774453235
Tatios <erik@gidlunds.net>
2013-05-23 15:13:13 +0200

    Added a db adapter
    Also updated searchtagactivity to talk through the dbadapter as well as
    adding an animation when changing activities

--- src/com/openmap/grupp1/SearchTagActivity.java
+++ src/com/openmap/grupp1/SearchTagActivity.java
@@ -1,186 +1,159 @@
 package com.openmap.grupp1;
- 
+
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
- 
+
 public class SearchTagActivity extends Activity implements SearchView.OnQueryTextListener,
 SearchView.OnCloseListener {
- 
-    private ListView listViewSearched;
-    private ListView listViewAdded;
-    private SearchView searchView;
-    private TagsDbAdapter mDbHelper;
-    private ArrayList<String> addedTags = new ArrayList<String>();
-    private ArrayAdapter<String> addedTagsAdapter;
+
+	private ListView listViewSearched;
+	private ListView listViewAdded;
+	private SearchView searchView;
+	private TagDbRequestTask mDbHelper;
+	private ArrayList<String> addedTags = new ArrayList<String>();
+	private ArrayAdapter<String> addedTagsAdapter;
+	private ArrayList<String> searchedTags = new ArrayList<String>();
+	private ArrayAdapter<String> searchedTagsAdapter;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.searchtagview);
+		overridePendingTransition(R.anim.map_out,R.anim.other_in);
+
+		listViewSearched = (ListView) findViewById(R.id.list_searched);
+		listViewAdded = (ListView) findViewById(R.id.list_added);
+
+		addedTagsAdapter =      
+				new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, addedTags);
+		listViewAdded.setAdapter(addedTagsAdapter);
+		// Define the on-click listener for listViewSearched
+		listViewSearched.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+				//Get the tag at the clicked position
+				String tag = (String) listViewSearched.getItemAtPosition(position);
+
+				//Adds the tag to listViewAdded to the right if it doesn't exist in it and removes it from listViewSearched
+				if(!addedTags.contains(tag)) {
+					addedTags.add(tag);
+					addedTagsAdapter.notifyDataSetChanged();
+					searchedTags.remove(tag);
+					searchedTagsAdapter.notifyDataSetChanged();
+				}
+			}
+		});
+
+		// Define the on-click listener for listViewAdded
+		listViewAdded.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				
+				//Get the tag at the clicked position
+				String removeItem = (String) listViewAdded.getItemAtPosition(position);
+				
+				//Adds the tag to listViewSearched to the left if it doesn't exist in it and removes it from listViewAdded
+				if(!searchedTags.contains(removeItem)) {
+					addedTags.remove(removeItem);
+					addedTagsAdapter.notifyDataSetChanged();
+					searchedTags.add(removeItem);
+					searchedTagsAdapter.notifyDataSetChanged();
+				}
+			}
+		});
 
 
 
 
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.searchtagview);
- 
-        
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.searchtagmenu, menu);
+		ActionBar ab = getActionBar();
+		ab.setDisplayShowTitleEnabled(false);
+		ab.setDisplayShowHomeEnabled(false);
 
- 
-        listViewSearched = (ListView) findViewById(R.id.list_searched);
-        listViewAdded = (ListView) findViewById(R.id.list_added);
-
-        
-        mDbHelper = new TagsDbAdapter(this);
-        mDbHelper.open();
- 
-        //Clean all tags
-        mDbHelper.deleteAllTags();
-        //Add some tags as a sample
-        mDbHelper.createTag("Fotbollsmatch");
-        mDbHelper.createTag("KLUBBA");
-        mDbHelper.createTag("Konsert");
-        mDbHelper.createTag("Tjena");
-        mDbHelper.createTag("Plats");
-        mDbHelper.createTag("Fest");
-        mDbHelper.createTag("Party");
-        mDbHelper.createTag("Fotboll");
-        mDbHelper.createTag("Hockey-VM");
-        
-    	addedTagsAdapter =      
-      	         new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, addedTags);
-      	         listViewAdded.setAdapter(addedTagsAdapter);
-        // Define the on-click listener for listViewSearched
-        listViewSearched.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Get the cursor, positioned to the corresponding row in the result set
-                Cursor cursor = (Cursor) listViewSearched.getItemAtPosition(position);
-                
-                // Get the tag from this row in the database.
-                String tag = cursor.getString(cursor.getColumnIndexOrThrow("tag"));
-
-                //Check if the Layout already exists
-                LinearLayout tagLayout = (LinearLayout)findViewById(R.id.tagLayout);
-                if(tagLayout == null){
-                    //Inflate the Tag Information View 
-                    LinearLayout rightLayout = (LinearLayout)findViewById(R.id.rightLayout);
-                    View tagInfo = getLayoutInflater().inflate(R.layout.searchviewaddedtags, rightLayout, false);
-                    rightLayout.addView(tagInfo);
-                }
-
-                //Adds the tag to addedListview to the right if it doesn't exist
-                if(!addedTags.contains(tag)) {
-                	addedTags.add(tag);
-                	addedTagsAdapter.notifyDataSetChanged();
-                }
-            }
-        });
-        
-        // Define the on-click listener for listViewAdded
-        listViewAdded.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            	String removeItem = (String) listViewAdded.getItemAtPosition(position);
-            	addedTags.remove(removeItem);
-            	addedTagsAdapter.notifyDataSetChanged();
-            }
-        });
-        
-        
-        
- 
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.searchtagmenu, menu);
-        ActionBar ab = getActionBar();
-        ab.setDisplayShowTitleEnabled(false);
-        ab.setDisplayShowHomeEnabled(false);
-        
-        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setIconifiedByDefault(false);
-        searchView.setOnQueryTextListener(this);
-        searchView.setOnCloseListener(this);
-        searchView.setQueryHint("Search tags");
-        searchView.requestFocus();
+		searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+		searchView.setIconifiedByDefault(false);
+		searchView.setOnQueryTextListener(this);
+		searchView.setOnCloseListener(this);
+		searchView.setQueryHint("Search tags");
+		searchView.requestFocus();
 
 
-        return true;
-    
-    }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-    case R.id.btn_add_search:
-    	//add filter function here, call loadMarkers in some way
-    	//send the arraylist addedTags here 
-    	return true;
-    default:
-        return super.onOptionsItemSelected(item);
-        }}
- 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mDbHelper  != null) {
-            mDbHelper.close();
-        }
-    }
- 
-    public boolean onQueryTextChange(String newText) {
-    	showResults(newText + "*");
-        return false;
-    }
- 
-    public boolean onQueryTextSubmit(String query) {
-        showResults(query + "*");
-        return false;
-    }
- 
-    public boolean onClose() {
-        showResults("");
-        return false;
-    }
-    
-    
+		return true;
 
-    
- 
-    private void showResults(String query) {
- 
-        Cursor cursor = mDbHelper.searchTag((query != null ? query.toString() : "@@@@"));
- 
-        if (cursor == null) {
-            //
-        } else {
-            // Specify the columns we want to display in the result
-            String[] from = new String[] {
-                    TagsDbAdapter.KEY_TAG};   
- 
-            // Specify the Corresponding layout elements where we want the columns to go
-            int[] to = new int[] {     R.id.stag};
- 
-            // Create a simple cursor adapter for the definitions and apply them to the ListView
-            SimpleCursorAdapter searchResult = new SimpleCursorAdapter(this,R.layout.searchviewsearchresult, cursor, from, to, 0);
-            listViewSearched.setAdapter(searchResult);
+	}
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.btn_add_search:
+			//add filter function here, call loadMarkers in some way
+			//send the arraylist addedTags here 
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}}
 
-        }
-    }
- 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	public boolean onQueryTextChange(String newText) {
+		showResults(newText);
+		return false;
+	}
+
+	public boolean onQueryTextSubmit(String query) {
+		showResults(query);
+		return false;
+	}
+
+	public boolean onClose() {
+		showResults("");
+		return false;
+	}
+
+
+
+
+
+	private void showResults(String query) {
+
+		try {
+			mDbHelper = new TagDbRequestTask();
+			searchedTags = mDbHelper.getTagArray(query);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (searchedTags != null && !searchedTags.isEmpty() && searchedTags.get(0) != null) {
+			searchedTagsAdapter =      
+					new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, searchedTags);
+			listViewSearched.setAdapter(searchedTagsAdapter);
+		} 
+		else {
+			//
+		}
+	}
+
 
 }
---
+++ src/com/openmap/grupp1/TagDbRequestTask.java
@@ -0,0 +1,82 @@
+package com.openmap.grupp1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class TagDbRequestTask extends AsyncTask<Void, Void, ArrayList<String>>{
+	private final String url = "http://129.16.205.204/php_mysql/tagRequest.php";
+	private List<NameValuePair> parameters;
+
+	public TagDbRequestTask(){
+		parameters = new ArrayList<NameValuePair>();
+
+	}
+
+	public ArrayList<String> getTagArray(String tagString) throws InterruptedException, ExecutionException{
+		parameters.add(new BasicNameValuePair("tag", "request"));
+		parameters.add(new BasicNameValuePair("tagName", tagString));
+		this.execute();
+		return this.get();
+	}
+	
+	public void addTag(String tagString){
+		parameters.add(new BasicNameValuePair("tag", "add"));
+		parameters.add(new BasicNameValuePair("tagName", tagString));
+		this.execute();
+	}
+
+	public ArrayList<String> requestTagArray() throws Exception{
+		HttpClient httpClient = new DefaultHttpClient();//used to execute post
+		HttpPost httpPost = new HttpPost(url);
+		httpPost.setEntity(new UrlEncodedFormEntity(parameters));
+
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		String responseBody = httpClient.execute(httpPost, responseHandler);
+		
+		JSONArray jArray = new JSONArray(responseBody);
+		JSONObject json_data = new JSONObject();
+		ArrayList<String> tagArray = new ArrayList<String>();
+		String tagName;
+		
+		for (int i=0; i<jArray.length(); i++){
+			json_data = jArray.getJSONObject(i);
+			tagName = json_data.getString("tagName");
+			tagArray.add(tagName);
+		}
+
+
+		return tagArray;
+		
+		
+	}
+
+	@Override
+	protected ArrayList<String> doInBackground(Void... v) {
+		try {
+			return requestTagArray();			
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	// onPostExecute displays the results of the AsyncTask.
+	@Override
+	protected void onPostExecute(ArrayList<String> result) {
+		
+	}
+	
+}
