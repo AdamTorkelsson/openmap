e298b342b81f3108e99d84367622942dd701320f
paer <paer.linder@gmail.com>
2013-05-29 10:17:50 +0200

    Class checkdburltask to check if database is available. Added to splash screenactivity to check this on start.

--- src/com/openmap/grupp1/SplashScreenActivity.java
+++ src/com/openmap/grupp1/SplashScreenActivity.java
@@ -1,42 +1,53 @@
 package com.openmap.grupp1;
 
+import com.openmap.grupp1.database.CheckDBUrlTask;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 /*
  * A splashscreen for when the user starts the program. 
  * Is active for 3 seconds
  */
 public class SplashScreenActivity extends Activity {
-    private long ms=0;
-    private long splashTime=3000;
-    private boolean splashActive = true;
-    private boolean paused=false;
-   
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	private long ms=0;
+	private long splashTime=3000;
+	private boolean splashActive = true;
+	private boolean paused=false;
 
-        //Hides the titlebar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.splashscreen);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        Thread mythread = new Thread() {
-            public void run() {
-                try {
-                    while (splashActive && ms < splashTime) {
-                        if(!paused)
-                            ms=ms+100;
-                        sleep(100);
-                    }
-                } catch(Exception e) {}
-                finally {
-                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            }
-        };
-        mythread.start();
-    }
+		//Hides the titlebar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splashscreen);
+
+		Thread mythread = new Thread() {
+			public void run() {
+				try {
+					while (splashActive && ms < splashTime) {
+						if(!paused)
+							ms=ms+100;
+						sleep(100);
+					}
+				} catch(Exception e) {}
+				finally {
+					//Check if database is available
+					boolean conn = new CheckDBUrlTask().tryDBConnection();
+					if (!conn)
+						Log.d("Database:","connection not available!");
+						//What will happen!? 
+					else{
+						Log.d("Database:","connection available!");
+						Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+						startActivity(intent);
+					}
+				}
+			}
+		};
+		mythread.start();
+	}
 }
